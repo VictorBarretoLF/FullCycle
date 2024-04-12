4d41249eefe79d34a9273376fbdd26c7ecd74191
Victor Barreto <victorbarretolins@gmail.com>
2024-04-11 21:45:19 -0300

    feat: implementando update de uma categoria

--- fc3-admin-do-catalogo/domain/src/main/java/com/fullcycle/admin/catalogo/domain/exceptions/DomainException.java
+++ fc3-admin-do-catalogo/domain/src/main/java/com/fullcycle/admin/catalogo/domain/exceptions/DomainException.java
@@ -4,20 +4,25 @@ import com.fullcycle.admin.catalogo.domain.validation.Error;
 
 import java.util.List;
 
-public class DomainException extends RuntimeException {
+public class DomainException extends NoStacktraceException {
 
     private final List<Error> errors;
 
-    private DomainException(final List<Error> anErrors){
-        super("", null, true, false);
+    protected DomainException(final String aMessage, final List<Error> anErrors) {
+        super(aMessage);
         this.errors = anErrors;
     }
 
-    public static DomainException with(final List<Error> anErros){
-        return new DomainException(anErros);
+    public static DomainException with(final Error anErrors) {
+        return new DomainException(anErrors.message(), List.of(anErrors));
+    }
+
+    public static DomainException with(final List<Error> anErrors) {
+        return new DomainException("", anErrors);
     }
 
     public List<Error> getErrors() {
         return errors;
     }
+
 }
